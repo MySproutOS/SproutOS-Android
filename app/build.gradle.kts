447cbad276aba52ba400f3c02b60cbfb6a02db6f
Andrew Chen Wang <acwangpython@gmail.com>
2026-09-02 17:54:18 -0400

    Prevent duplicate personal sites from crashing the app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.sproutos.store"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.3.1"
+        versionCode = 9
+        versionName = "0.3.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["usesCleartextTraffic"] = "false"
     }
