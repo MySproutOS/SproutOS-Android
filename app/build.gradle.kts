e29fef201bb2486c3a8785d617175669bdfa3ae6
Andrew Chen Wang <acwangpython@gmail.com>
2026-09-02 16:23:20 -0400

    Prepare Android client 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.sproutos.store"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.2.3"
+        versionCode = 7
+        versionName = "0.3.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["usesCleartextTraffic"] = "false"
     }
